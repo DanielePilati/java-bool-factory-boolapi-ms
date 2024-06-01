2dc5cabc30552ece34432225a6920fd97c324702
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-01 15:13:18 +0200

    init commit

---
+++ src/main/java/com/boolfactory/BoolAPI/BoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.boolfactory.BoolAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boolfactory/BoolAPI/BoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boolfactory.BoolAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
